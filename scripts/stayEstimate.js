97eda50066edfbbffc26d8463fd1cbdd877508f2
Candice Milburn <candicemilburn@hotmail.com>
2024-05-07 15:57:42 -0500

    saving progress

--- scripts/stayEstimate.js
+++ scripts/stayEstimate.js
@@ -2,17 +2,36 @@
 
 console.log("hello fellow")
 
-window.onload = function(){
+window.onload = function () {
     let estimateForm = document.querySelector("#estimateForm");
-    
+
     estimateForm.addEventListener("submit", stayEstimate);
 
 }
-
-function stayEstimate(event){
+function stayEstimate(event) {
     event.preventDefault();
 
     let estimateForm = event.target;
 
-    console.log("you submitted the form");
+    let roomTotal = Number(estimateForm.numOfNights.value) * getRoomRate(estimateForm.checkInDate.value, estimateForm.roomType.value);
+
+    console.log(roomTotal);
+
+    console.log(estimateForm.checkInDate.value)
+
+
+
+    function getRoomRate(checkInDate, roomType) {
+//got this from listening to instructor
+        let checkInDatedate = new Date(checkInDate).getMonth() + 1
+    //rate of 
+        let rate = estimateForm.roomType.value
+        if (roomType === "suite") {
+            return 210;
+        } if (roomType === "queen") {
+            return 150;
+        } if (roomType === "king") {
+            return 150;   }
+        return roomTotal
+    }
 }
